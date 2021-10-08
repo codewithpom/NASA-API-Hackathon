f819360c7ddd558880d3c6bb9aba1d6a287eaa81
Padmashree Jha <codewithpom@gmail.com>
2021-10-08 21:57:12 +0530

    Bookmark
    Added row column to bookmarks system to make it of the same size.

--- src/pages/BookMarks.js
+++ src/pages/BookMarks.js
@@ -56,30 +56,38 @@ export default function BookMarks() {
     console.log(element)
     return (
         <div className="conatiner mt-4 pt-5 text-center" style={{ minHeight: "100vh" }}>
+            <h1>
+                Your boomarks
+            </h1>
+            <p>
+                Your bookmarks are synced with your account
+            </p>
+            <div className="row">
+                {
+                    Array(element)[0].map((link, index) => {
+                        console.log(link)
+                        return (
+                            <div className="col-lg-4">
+                                <div className="card" style={{ width: "18rem" }} key={index} id={index} link={link}>
+                                    <img src={`https://images-assets.nasa.gov/image/${link.split("/").slice(-1)}/${link.split("/").slice(-1)}~thumb.jpg`} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{decodeURI(link.split("/").slice(-1))}</h5>
+                                        <button className="btn btn-danger mx-3" onClick={() => remove_card(index)}>
+                                            <i className="fa fa-times" aria-hidden="true"></i>
+                                        </button>
 
-            {
-                Array(element)[0].map((link, index) => {
-                    console.log(link)
-                    return (
-
-                        <div className="card" style={{ width: "18rem" }} key={index} id={index} link={link}>
-                            <img src={`https://images-assets.nasa.gov/image/${link.split("/").slice(-1)}/${link.split("/").slice(-1)}~thumb.jpg`} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{decodeURI(link.split("/").slice(-1))}</h5>
-                                <button className="btn btn-danger mx-3" onClick={() => remove_card(index)}>
-                                    <i className="fa fa-times" aria-hidden="true"></i>
-                                </button>
-
-                                <Link to={link.replace(/^[a-z]{4,5}:\/{2}[a-z]{1,}:[0-9]{1,4}.(.*)/, '$1').replace("#", "")}>
-                                    <button className="btn btn-primary">
-                                        Details
-                                    </button>
-                                </Link>
+                                        <Link to={link.replace(/^[a-z]{4,5}:\/{2}[a-z]{1,}:[0-9]{1,4}.(.*)/, '$1').replace("#", "")}>
+                                            <button className="btn btn-primary">
+                                                Details
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div >
     )
 }
